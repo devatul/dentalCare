9651785b702da0bdcffba7ad843b15a7830718fd
vikas <rohit.excel2011@gmail.com>
2018-08-02 18:23:53 +0530

    setting page in progress

--- src/components/SettingPage.js
+++ src/components/SettingPage.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Tabs, Tab, FormControl } from 'react-bootstrap';
+import { Tabs, Tab, FormControl, Button } from 'react-bootstrap';
 
 
 class SettingPage extends Component {
+    state = {
+        activeTab: 1
+    }
   render() {
     return (
       <div className="page-body-wrapper">
@@ -12,14 +15,22 @@ class SettingPage extends Component {
                 <h1>{this.props.data.title}</h1>
             </section>
             <section className="tabs-data">
-                <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
-                    <Tab eventKey={1} title="Billing">
-                        Billing
+                <Tabs defaultActiveKey={this.state.activeTab} id="uncontrolled-tab-example">
+                    <Tab eventKey={1} title="Billing" onClick={()=>this.setState({activeTab:1})}>
+                        <div className="billing">
+                            <div className="button-wrapper">
+                                <Button>Subscriptions</Button>
+                            </div>
+                            <div className="expendable-box expanded">
+                                <div className="header">Payment History</div>
+                                <div className="content">page content</div>
+                            </div>
+                        </div>
                     </Tab>
-                    <Tab eventKey={2} title="Deposit">
+                    <Tab eventKey={2} title="Deposit" onClick={()=>this.setState({activeTab:2})}>
                         Deposit
                     </Tab>
-                    <Tab eventKey={3} title="Clinic Setting">
+                    <Tab eventKey={3} title="Clinic Setting" onClick={()=>this.setState({activeTab:3})}>
                         Clinic Setting
                     </Tab>
                 </Tabs>
